b234596102f2506bcba4923a5dbedcee78d1c560
hieuitcz <124533474+hieuitcz@users.noreply.github.com>
2026-04-22 00:16:07 +0200

    Add files via upload

--- kid/sw.js
+++ kid/sw.js
@@ -1,16 +1,63 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = ['/kid/hd.txt'];
+const CACHE_NAME = 'kid-pwa-cache-v3';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './animals.json',
+  './manifest.json',
+  './hd.txt',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+const APP_SHELL_URLS = APP_SHELL.map(path => new URL(path, self.registration.scope).toString());
+const OFFLINE_FALLBACK_URL = new URL('./index.html', self.registration.scope).toString();
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(APP_SHELL_URLS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) return response;
+
+        return fetch(event.request)
+          .then(networkResponse => {
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            const cloned = networkResponse.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
+            return networkResponse;
+          })
+          .catch(() => {
+            if (event.request.mode === 'navigate') {
+              return caches.match(OFFLINE_FALLBACK_URL);
+            }
+            return new Response('', { status: 504, statusText: 'Gateway Timeout' });
+          });
+      })
   );
 });
